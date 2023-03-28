e1452935bd572cb24aecf78820736b7f414863ae
pexApred <120075705+pexApred@users.noreply.github.com>
2023-03-28 18:13:00 -0400

    added packages needed for application

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // TODO: Include packages needed for this application
-
+    const inquirer = require ('inquirer');
+    const fs = require('fs');
+    
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
