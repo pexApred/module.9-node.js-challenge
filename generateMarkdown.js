44d7478ee58fba131b5fd50430dd9ab73fbb3236
pexApred <120075705+pexApred@users.noreply.github.com>
2023-03-29 01:24:00 -0400

    completed function that returns licese section of readme

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -28,12 +28,52 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  } else {
+    return `## License 
+    
+    This project is licensed under the ${license} license. Click [here](${renderLicenseLink(license)}) for more information.`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
+  
+  ## Description
 
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ${renderLicenseSection(data.license)}
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage 
+
+  ${data.usage}
+  
+  ## Contributing 
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at $[${data.github}](https://github.com/${data.github}/)
 `;
 }
 
