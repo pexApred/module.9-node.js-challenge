fe337a4e21dc259a62d1a2061bede326f28777fb
pexApred <120075705+pexApred@users.noreply.github.com>
2023-03-29 00:55:51 -0400

    Created function that returns license link

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -14,7 +14,17 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return 'https://opensource.org/license/mit/'
+  } else if (license === 'GPL 3.0') {
+    return 'https://www.gnu.org/licenses/gpl-3.0'
+  } else if (license === 'BSD 3-Clause') {
+    return 'https://opensource.org/licenses/BSD-3-Clause'
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
